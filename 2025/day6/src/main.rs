5ff1ae1f67a9c97658ff49151f158cdb7cf9832e
Clara Shoemaker <cshoemaker@seacorp.com>
2026-07-24 11:11:40 -0400

    reformat for day 6 part 2

--- 2025/day6/src/main.rs
+++ 2025/day6/src/main.rs
@@ -1,21 +1,38 @@
 use helper_scripts::get_puzzle_input;
 
 fn main() {
+    let part_1 = false;
+    let part_1 = true;
+
     let mut opers: Vec<char> = Vec::new();
     let mut problems: Vec<Vec<u64>> = Vec::new();
-    let succ = load_input(&mut problems, &mut opers);
-    // let succ = load_dummy_input(&mut problems, &mut opers);
 
-    if !succ {
-        println!("Exiting...");
-        return;
+    if part_1 {
+        let succ = load_input(&mut problems, &mut opers);
+        // let succ = load_dummy_input(&mut problems, &mut opers);
+
+        if !succ {
+            println!("Exiting...");
+            return;
+        }
+    } else {
+        let succ = load_part_2_input(&mut problems, &mut opers);
+        // let succ = load_part_2_dummy_input(&mut problems, &mut opers);
+
+        if !succ {
+            println!("Exiting...");
+            return;
+        }
+
     }
-
-    part_1(opers, &problems);
-    // part_2();
+    
+    fold(problems, opers);
 }
 
-fn part_1(opers: Vec<char>, problems : &Vec<Vec<u64>>) {
+fn fold(problems: Vec<Vec<u64>>, opers: Vec<char>) {
+
+
+
     let mut sum:u64 = 0;
     for idx in 0..problems.len() {
         if opers[idx] == '+' {
@@ -27,11 +44,6 @@ fn part_1(opers: Vec<char>, problems : &Vec<Vec<u64>>) {
 
     println!("Sum is {}", sum);
 }
-
-fn part_2() {
-
-}
-
 
 // get input
 fn load_input(problems : &mut Vec<Vec<u64>>, opers : &mut Vec<char>) -> bool {
@@ -80,6 +92,29 @@ fn load_input(problems : &mut Vec<Vec<u64>>, opers : &mut Vec<char>) -> bool {
 fn load_dummy_input(problems : &mut Vec<Vec<u64>>, ids : &mut Vec<char>) -> bool {
     problems.extend_from_slice(&[vec![123,45,6],vec![328,64,98],vec![51,387,215],vec![64,23,314]]);
     ids.extend(['*','+','*','+']);
+
+    true
+}
+
+// get input
+fn load_part_2_input(problems : &mut Vec<Vec<u64>>, opers : &mut Vec<char>) -> bool {
+    let mut data: String = String::from("");
+
+    if false == get_puzzle_input::fetch_input_string("https://adventofcode.com/2025/day/6/input", &mut data) {
+        println!("{}", data);
+        return false;
+    } else {
+        println!("Successful read of data");
+    }
+
+    let lines: Vec<&str> = data.split('\n').collect();
+    
+    true
+}
+
+fn load_part_2_dummy_input(problems : &mut Vec<Vec<u64>>, ids : &mut Vec<char>) -> bool {
+    problems.extend_from_slice(&[vec![4,431,623],vec![175,581,32],vec![8,248,369],vec![356,24,1]]);
+    ids.extend(['+','*','+','*']);
 
     true
 }
